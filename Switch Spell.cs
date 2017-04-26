7ed2b11972d025d02c350ded23dfe52ed83a696d
SeanConway512 <Sean.Conway@students.ittralee.ie>
2017-04-26 20:58:50 +0100

    Switch Spells

---
+++ Switch Spell.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchSpell : MonoBehaviour {
+
+    public GameObject weapon01;
+    public GameObject weapon02;
+
+	// Use this for initialization
+	void Start () {
+        weapon02.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            switchWeaponsPlease();
+        }
+	}
+    void switchWeaponsPlease()
+    {
+        if (weapon01.activeSelf)
+        {
+            weapon01.SetActive(false);
+            weapon02.SetActive(true);
+        }
+        else
+        {
+            weapon01.SetActive(true);
+            weapon02.SetActive(false);
+        }
+    }
+}
