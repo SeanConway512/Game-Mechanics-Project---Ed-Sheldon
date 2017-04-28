5d1eb1b76de2f13bee694943a2265a17fc87450b
SeanConway512 <Sean.Conway@students.ittralee.ie>
2017-04-28 04:54:51 +0100

    Lightning spell

---
+++ Lightning Spell.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell : MonoBehaviour
+{
+    
+    
+
+    public GameObject spell;
+    public Transform ShootPoint;
+    private float fireRate = 1f;
+
+    private float nextFire;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(spell, ShootPoint.position, ShootPoint.rotation);
+        }
+    }
+}
+   
+
